cff03e1d02b57bf0865ba5856998c3c19f506923
Adam McKerlie <adammckerlie@gmail.com>
2020-04-17 09:59:59 -0400

    v1.0
    * Handles a single, regular length hilt and blade
    * Handles duplicate lightsabers
    * Offsets for buttons started

---
+++ src/lightsaber.py
@@ -0,0 +1,57 @@
+import os, random
+from PIL import Image
+from manifest import MANIFEST
+
+IMAGE_PATH = "../images"
+BLADE_PATH = "{}/blades".format(IMAGE_PATH)
+HILT_PATH = "{}/hilts".format(IMAGE_PATH)
+OUTPUT_PATH = "{}/lightsabers".format(IMAGE_PATH)
+
+def fetch_lightsaber_parts():
+    blade = "{}/{}".format(BLADE_PATH, random.choice(os.listdir(BLADE_PATH)))
+    hilt = "{}/{}".format(HILT_PATH, random.choice(os.listdir(HILT_PATH)))
+
+    return (blade, hilt)
+
+def fetch_name(path):
+    return path.split('/')[-1]
+
+def resize_image(image):
+    image_w, image_h = image.size
+    image = image.resize((image_w*4, image_h*4), Image.ANTIALIAS)
+    return image, image.size
+
+def get_hilt_offset(hilt):
+    hilt_name = hilt.split('.')[0]
+    return MANIFEST['hilt'][hilt_name]['offsets']['blade']
+
+def generate_lightsaber():
+    hilt_name = blade_name = '.'
+    output_filename = ''
+
+    while blade_name.startswith('.') == True or hilt_name.startswith('.') == True or f"{output_filename}.png" in os.listdir(OUTPUT_PATH):
+        blade_path, hilt_path = fetch_lightsaber_parts()
+        blade_name = fetch_name(blade_path)
+        hilt_name = fetch_name(hilt_path)
+
+        # Create the filename here so we can check for uniqueness
+        output_filename = f"{hilt_name.split('.')[0]}{blade_name.split('.')[0]}"
+   
+    blade = Image.open(blade_path, 'r')
+    blade, (blade_w, blade_h) = resize_image(blade)
+   
+    hilt = Image.open(hilt_path, 'r')
+    hilt, (hilt_w, hilt_h) = resize_image(hilt)
+    
+    img = Image.new("RGB", (1024, 512), (255, 255, 255))
+    bg_w, bg_h = img.size
+
+    hilt_offset = get_hilt_offset(hilt_name)
+    blade_offset = ((bg_w - blade_w) // 2, (bg_h - blade_h - hilt_h + hilt_offset))
+    hilt_offset = ((bg_w - hilt_w) // 2, bg_h-hilt_h)
+
+    img.paste(blade, blade_offset, mask=blade)
+    img.paste(hilt, hilt_offset, mask=hilt)
+    img.save("{}/{}.png".format(OUTPUT_PATH, output_filename))
+
+generate_lightsaber()
---
+++ src/manifest.py
@@ -0,0 +1,31 @@
+MANIFEST = {
+    "hilt": {
+        "h1": {
+            "offsets": {
+                "blade": 0,
+                "button": {
+                    "x": (),
+                    "y": ()
+                }
+            }
+        },
+        "h2": {
+            "offsets": {
+                "blade": 12,
+                "button": {
+                    "x": (),
+                    "y": ()
+                }
+            }
+        },
+         "h3": {
+            "offsets": {
+                "blade": 0,
+                "button": {
+                    "x": (),
+                    "y": ()
+                }
+            }
+        }
+    }
+}
